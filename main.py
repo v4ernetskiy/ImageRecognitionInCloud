e228f24854555c369b230cfd08eb2a1397637fcf
v4ernetskiy <v4ernetskiy@urfu.ru>
2023-03-04 17:06:59 +0500

    fix title page view

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import io
+import numpy as np
 import streamlit as st
 from PIL import Image
-import numpy as np
 from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
-
+from tensorflow.keras.preprocessing import image
 
 st.cache_resource()
-# allow_output_mutation=True
 
+
+# allow_output_mutation=True
 
 def load_model():
     return EfficientNetB0(weights='imagenet')
@@ -24,7 +24,7 @@ def preprocess_image(img):
 
 
 def load_image():
-    uploaded_file = st.file_uploader(label='SELECT AN IMAGE FOR RECOGNITION')
+    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания ')
     if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
         st.image(image_data)
@@ -40,11 +40,16 @@ def print_predictions(preds):
 
 
 model = load_model()
-st.title('IMAGE CLASSIFICATION IN STREAMLIT CLOUD')
+st.title('Распознование изображений в облаке STREAMLIT')
+st.markdown('Проект выполнен в рамках программы **Разработка приложений ИИ**')
+st.text('Тарасов Д. А., Чернецкий И. В., Веревочников А. В., Гаймбихнер В. В., Крылосов А. В.')
+
 img = load_image()
-result = st.button('RECOGNIZE THE IMAGE')
+result = st.button('Распознать изображение')
 if result:
-    x = preprocess_image(img)
-    preds = model.predict(x)
-    st.write('RECOGNITION RESULT -->')
-    print_predictions(preds)
+    with st.spinner('Подождите немного...'):
+        x = preprocess_image(img)
+        preds = model.predict(x)
+        st.write('Результат -->')
+        print_predictions(preds)
+    st.success('Готово!')
